cafadd6a5a63e342b3680d034868ec68da42e20f
Sergio Schvezov <sergio.schvezov@canonical.com>
2019-09-09 10:54:39 -0300

    deb: transition to snap package

--- snapcraft/internal/project_loader/grammar/typing.py
+++
@@ -1,6 +0,0 @@
-from typing import Any, Dict, List, Union
-
-Grammar = List[Union[str, Dict[str, Any]]]
-CallStack = List["Statement"]
-
-from ._statement import Statement  # noqa: F401
--- units.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-unittest.main(
-    "snapcraft.tests.unit.commands.test_build", argv=["BuildCommandTestCase"]
-)  # noqa
